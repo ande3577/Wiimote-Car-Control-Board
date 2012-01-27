c20368886683a79db70a5e426c77b35bca2f5985
David S Anderson <ande3577@gmail.com>
2012-01-26 19:06:19 -0600

    added copy of gpl and header info

--- controllerboardavr/modules/sensor.c
+++ controllerboardavr/modules/sensor.c
@@ -1,13 +1,28 @@
+/**
+ * @author David S Anderson
+ *
+ *
+ * Copyright (C) 2011 David S Anderson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "timestamp.h"
 #include "sensor.h"
 #include "config.h"
 #include "sleep.h"
 #include "hw.h"
-
-/*!
- @file
- \brief Routines for setting up and reading ADC.
- */
 
 volatile uint8_t current_sensor_channel;
 #if NUMBER_OF_SENSOR_CHANNELS != SENSOR_REQUIRED_CHANNELS
